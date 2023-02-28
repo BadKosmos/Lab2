80dd1c35fb5a683783a9b4818a5430a13c2ebe10
BadKosmos <thefest188@mail.ru>
2023-02-28 20:02:47 +0300

    proc 1-4 ( 5/6/7 )

--- main.py
+++ main.py
@@ -40,3 +40,31 @@ def proc4():
             print('Оранжевый')
     else:
         print('Цвет введен неверно')
+
+'''
+def proc5():
+    n = int(input())
+    result = []
+    for i in range(n):
+        result.append(input())
+    word = ' '.join(result)
+    print(word)
+
+def proc2():
+    words = []
+    while (new_word := str(input())) != "stop":
+        words.append(new_word)
+    print(" ".join(words))
+
+def proc3():
+    words = []
+    while (new_word := str(input())) !="stop":
+        if "ф" in new_word or "Ф" in new_word:
+            print('Ого, это редкое слово!')
+        else:
+            print('Это обычно слово')
+        words.append(new_word)
+    print(" ".join(words))
+proc3()
+
+'''
